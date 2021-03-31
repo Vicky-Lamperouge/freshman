f3687a0fa89aca832fd8a88d7d12b27c0b826a0e
Vicky-Lamperouge <79582466+Vicky-Lamperouge@users.noreply.github.com>
2021-03-31 19:32:18 +0800

    Add files via upload

---
+++ 2020310827/2020310827 homework1.py
@@ -0,0 +1,20 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 123+789
+912
+>>> 5**5
+3125
+>>> a=[1,2,3,4,]
+>>> a[0]
+1
+>>> b={7,8,'a','s'}
+>>> b
+{8, 'a', 's', 7}
+>>> 'a' in b
+True
+>>> tel={"Jessica":23,"Mary":44}
+>>> print(tel)
+{'Jessica': 23, 'Mary': 44}
+>>> type(tel)
+<class 'dict'>
+>>> 
